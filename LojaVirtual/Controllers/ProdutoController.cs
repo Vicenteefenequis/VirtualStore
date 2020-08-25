b6231f8599120c507bde14d38e47f33c13a8458e
Vicente Nascimento <49836207+Vicenteefenequis@users.noreply.github.com>
2020-08-24 21:10:16 -0300

    :art: Structure projet MVC

---
+++ LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,35 @@
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        /*
+         * Action Result
+         * IActionResult
+         */
+        public ActionResult Visualizar()
+        {
+            Produto produto  = GetProduto();
+            return View(produto);
+           // return new ContentResult() { Content = "" , ContentType = "text/html"};
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "Playstation 5",
+                Descricao = "Jogue em 8 dimensoes",
+                Valor = 2000.00M
+            };
+        }
+    }
+}
