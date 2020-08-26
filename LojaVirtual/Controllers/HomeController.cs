e261fb864ca9c7365e9669423099164aeb0ee504
Vicente Nascimento <49836207+Vicenteefenequis@users.noreply.github.com>
2020-08-26 17:04:15 -0300

    :zap:  contact form validation

--- LojaVirtual/Controllers/HomeController.cs
+++ LojaVirtual/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using LojaVirtual.Libraries.Email;
 using LojaVirtual.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace LojaVirtual.Controllers
 {
@@ -28,9 +30,29 @@ namespace LojaVirtual.Controllers
                 contato.Email = HttpContext.Request.Form["email"];
                 contato.Texto = HttpContext.Request.Form["texto"];
 
-                ContatoEmail.EnviarContatoPorEmail(contato);
+                var listaMensagens = new List<ValidationResult>();
+                var contexto = new ValidationContext(contato);
+                bool isValid = Validator.TryValidateObject(contato, contexto, listaMensagens,true);
 
-                ViewData["MSG_SUCESSO"] = "Mensagem de Contato enviado com sucesso!";
+                if (isValid)
+                {
+                    ContatoEmail.EnviarContatoPorEmail(contato);
+
+                    ViewData["MSG_SUCESSO"] = "Mensagem de Contato enviado com sucesso!";
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var texto in listaMensagens)
+                    {
+                        sb.Append(texto.ErrorMessage + "<br />");
+                    }
+
+                    ViewData["MSG_ERROR"] = sb.ToString();
+                }
+                
+
+              
 
             }
             catch (Exception ex2)
--- LojaVirtual/Models/Contato.cs
+++ LojaVirtual/Models/Contato.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using LojaVirtual.Libraries.Lang;
 
 namespace LojaVirtual.Models
 {
     public class Contato
     {
+        [Required(ErrorMessageResourceType = typeof(Mensagem),ErrorMessageResourceName = "MSG_ERROR001")]
+        [MinLength(4, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_ERROR002")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_ERROR001")]
+        [EmailAddress(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_ERROR004")]
         public string Email { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_ERROR001")]
+        [MinLength(10, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_ERROR002")]
+        [MaxLength(1000, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_ERROR003")]
         public string Texto { get; set; }
     }
 }
