c9a0d0df713d21a50b06648ba6f2a21e065cd1f7
Vicente Nascimento <49836207+Vicenteefenequis@users.noreply.github.com>
2020-09-03 21:59:27 -0300

    :lipstick: area colaborador view

--- LojaVirtual/Areas/Colaborador/Controllers/HomeController.cs
+++ LojaVirtual/Areas/Colaborador/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LojaVirtual.Areas.Controlador.Controllers
 {
+    [Area("Colaborador")]
     public class HomeController : Controller
     {
         public IActionResult Login()
