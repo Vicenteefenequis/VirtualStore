9fd77130f160c746cb5f6a780fe362d20219ec65
Vicente Nascimento <49836207+Vicenteefenequis@users.noreply.github.com>
2020-09-21 22:32:19 -0300

    :sparkles: create validateHttpReferer for validation on delete and generate password

--- LojaVirtual/Areas/Colaborador/Controllers/ColaboradorController.cs
+++ LojaVirtual/Areas/Colaborador/Controllers/ColaboradorController.cs
@@ -33,6 +33,7 @@ namespace LojaVirtual.Areas.Colaborador.Controllers
         }
 
         [HttpGet]
+        [ValidateHttpReferer]
         public IActionResult GerarSenha(int id)
         {
 
@@ -93,6 +94,7 @@ namespace LojaVirtual.Areas.Colaborador.Controllers
             return View();
         }
         [HttpGet]
+        [ValidateHttpReferer]
         public IActionResult Excluir(int id)
         {
             _colaboradorRepository.Excluir(id);
